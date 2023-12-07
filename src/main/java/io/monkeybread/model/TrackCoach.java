ee4be939ce6aa274f1c01aa8d3fcbd273edde67e
Sady Doucoure <sadydoucoure@gmail.com>
2023-12-07 15:00:18 -0500

    @Lazy annotation in application.properties

--- src/main/java/io/monkeybread/model/TrackCoach.java
+++ src/main/java/io/monkeybread/model/TrackCoach.java
@@ -1,10 +1,8 @@
 package io.monkeybread.model;
 
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
-@Lazy
 public class TrackCoach implements Coach {
 
 	public TrackCoach() {
