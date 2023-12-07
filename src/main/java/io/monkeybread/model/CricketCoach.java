d2c6ea6dce8c229c71897dc903d14113992c5a5f
Sady Doucoure <sadydoucoure@gmail.com>
2023-12-07 15:36:01 -0500

    Component lifecycle @PostConstruct & PreDestroy

--- src/main/java/io/monkeybread/model/CricketCoach.java
+++ src/main/java/io/monkeybread/model/CricketCoach.java
@@ -3,11 +3,24 @@ package io.monkeybread.model;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 @Component
-@Scope("prototype")
+//@Scope("prototype")
 public class CricketCoach implements Coach {
 	public CricketCoach() {
 		System.out.println("In constructer: " + getClass().getSimpleName());
+	}
+
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println("doMyStartupStuff in: " + getClass().getSimpleName());
+	}
+
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println("doMyCleanupStuff in: " + getClass().getSimpleName());
 	}
 
 	@Override
